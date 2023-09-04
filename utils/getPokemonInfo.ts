c68f2334b3775de6f6b69b9151d74e7b7d9d27fd
facundobailo <facundo.bailo@gmail.com>
2023-09-04 19:53:05 -0300

    Agregue la funcion del ISR y del ISG

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -4,13 +4,21 @@ import { Pokemon } from "@/interfaces"
 
 export const getPokemonInfo = async( namerId: string ) => {
 
+
+  try {
+    
     const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ namerId }`)
-  
+    
     return {
       id: data.id,
       name: data.name,
       sprites: data.sprites
     }
+
+  } catch (error) {
+    return null
+  }
+
   
 
 }
